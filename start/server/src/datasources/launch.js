659a70aadab43a21cf9b9608353e4d973fad976f
Haris Hajdarevic <haris.hajdarevic@walter.ba>
2018-11-28 20:03:01 +0100

    3. Write your graph's resolvers [ starting... ]

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -11,14 +11,32 @@ class LaunchAPI extends RESTDataSource {
         return response && response.length ? response.map(launch => this.launchReducer(launch)) : [];
     }
 
+    async getLaunchById({ launchId }) {
+        const response = await this.get('launches', { flight_number: id });
+        return this.launchReducer(res[0]);
+    }
+
+    async getLaunchesByIds({ launchIds }) {
+        return Promise.all(
+            launchIds.map(launchId => this.getLaunchById({ launchId }))
+        )
+    }
+
     // https://www.apollographql.com/docs/tutorial/data-source.html
     launchReducer(launch) {
         return {
             id: launch.flight_number,
             site: launch.launch_site && launch.launch_site.site_name,
             mission: {
-                name:
-            }
+                name: launch.mission_name,
+                missionPatchSmall: launch.links.mission_patch_small,
+                missionPatchLarge: launch.links.mission_patch,
+            },
+            rocket: {
+                id: launch.rocket.rocket_id,
+                name: launch.rocket.rocket_name,
+                type: launch.rocket.rocket_type,
+            },
         }
     }
     // async getLaunchById()
